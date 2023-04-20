d617884f47434d32cd0f7d89afa703e97f12d358
alexdolg1967 <alexdolg1967@mail.ru>
2023-04-20 20:30:01 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,73 @@
+import "./_vendor";
+import vars from "./_vars";
+import "./_functions";
+import "./_components";
+import Swiper, { Navigation, Pagination } from 'swiper';
+  Swiper.use([Navigation, Pagination]);
+
+var ua = window.navigator.userAgent;
+var msie = ua.indexOf("MSIE ");
+var isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+function isIE() {
+  ua = navigator.userAgent;
+  var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+  return is_ie;
+}
+if (isIE()) {
+  document.querySelector("html").classList.add("ie");
+}
+if (isMobile.any()) {
+  document.querySelector("html").classList.add("_touch");
+}
+
+window.onload = function () {
+
+  const swiper = new Swiper('.new-arrivals__slider', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      567: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      }
+    },
+  });
+};
+
